7bc3610591504ea1fc22408665974f167d197797
ichigo-dev <ichigo@web-freework.com>
2021-07-03 18:19:34 +0900

    add: serializers

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,6 @@
+# ==============================================================================
+# app - serializser - user
+# ==============================================================================
+class UserSerializer < ApplicationSerializer
+  attributes :name, :created_at
+end
